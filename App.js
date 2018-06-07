633de26a341ce0e48a479173e3c9600a16352358
Isaac Guerreiro <isaac.guerreiro@itriad.org.br>
2018-06-07 15:05:51 -0400

    Create a map an introduce an pre-marker

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { MapView } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Começo do Buracos App!</Text>
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: -3.1067,
+          longitude: -60.0000,
+          latitudeDelta: 0.1422,
+          longitudeDelta: 0.1421,
+        }}
+      >
+        <MapView.Marker 
+          key={1}
+          coordinate={{latitude: -3.0425, longitude: -60.0048}}
+          title={"Grave"}
+          description={"Cidade Nova"}
+        />
+      </MapView>
     );
   }
 }
